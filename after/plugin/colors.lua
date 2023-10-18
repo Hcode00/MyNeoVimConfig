5eed0db560ff4565cd9e90137266191698cda417
Hcode00 <es-hazemahmed2025@alexu.edu.eg>
2023-10-18 17:59:30 +0300

    updated main theme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,5 +1,5 @@
 function ColorTheme (color)
-	color = color or "tokyonight-night"
+	color = color or "tokyonight"
 	vim.cmd.colorscheme(color)
 	vim.api.nvim_set_hl(0,"Normal", {bg = "None"})
 	vim.api.nvim_set_hl(0,"NormalFloat", {bg = "None"})
